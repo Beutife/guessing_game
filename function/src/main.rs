fda71b6762e3dcb7e4d16278c7d11c16c1967925
Beutife <104403355+Beutife@users.noreply.github.com>
2026-01-16 23:52:42 +0100

    function

--- function/src/main.rs
+++ function/src/main.rs
@@ -3,11 +3,16 @@ fn main() {
 
 
     another_function(6);
+
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function(x: i32){
     println!("Another function");
-    println!("The value of x is: {x}")
+    println!("The value of x is: {x}");
 }
 
 
+fn print_labeled_measurement(value: i32, unit_label: char){
+    println!("The measurement is: {value}{unit_label}")
+}
