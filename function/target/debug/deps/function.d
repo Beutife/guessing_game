317be05044f96cb03251704e2cedf9532ba35af6
Beutife <104403355+Beutife@users.noreply.github.com>
2026-01-16 23:45:22 +0100

    function

---
+++ function/target/debug/deps/function.d
@@ -0,0 +1,5 @@
+C:\Users\udebe\guessing_game\function\target\debug\deps\function.d: src\main.rs
+
+C:\Users\udebe\guessing_game\function\target\debug\deps\function.exe: src\main.rs
+
+src\main.rs:
---
+++ function/target/debug/function.d
@@ -0,0 +1,1 @@
+C:\Users\udebe\guessing_game\function\target\debug\function.exe: C:\Users\udebe\guessing_game\function\src\main.rs
